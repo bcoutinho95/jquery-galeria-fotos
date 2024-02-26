de4fa2ef9e592cb84691cf0c9a274ce864543352
Bruno Coutinho <bmferreira298@gmail.com>
2024-02-26 08:51:37 -0300

    commit inicial

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-
-    //Em javacript add evento
-    /*document.querySelector('header button').addEventListener('click', function(){
-        alert("Expandir formulário")
-    })*/
-
-    // no JQuery
-    /*$('header button').click(function(){
-        alert("Expandir formulário")
-    })*/
-
-
-
 $(document).ready(function() {
     $('header button').click(function() {
         $('form').slideDown();
@@ -22,18 +9,18 @@ $(document).ready(function() {
 
     $('form').on('submit', function(e) {
         e.preventDefault();
-        const enderecoDaNovaImagem = $(`#endereco-imagem-nova`).val();
-        const novoItem = $(`<li style="display: none"></li>`);
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
         $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
         $(`
             <div class="overlay-imagem-link">
-                <a href ="${enderecoDaNovaImagem}" target:"_blank" title="Ver imagem em tamanho real
+                <a href ="${enderecoDaNovaImagem}" target:"_blank" title="Ver imagem em tamanho real">
                     Ver imagem em tamanho real
                 </a>
             </div>
         `).appendTo(novoItem);
-        $(novoItem).appendTo(`ul`);
+        $(novoItem).appendTo('ul');
         $(novoItem).fadeIn(1000);
-        $(`#endereco-imagem-nova`).val(``)
+        $('#endereco-imagem-nova').val('')
     })
 })
